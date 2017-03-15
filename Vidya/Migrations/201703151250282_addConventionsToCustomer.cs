ee9e7e0ac03884b2661f2685bba16e6f80101637
DevOps\Developer <bobbanovski@gmail.com>
2017-03-15 23:54:40 +1100

    edit customer

---
+++ Vidya/Migrations/201703151250282_addConventionsToCustomer.cs
@@ -0,0 +1,18 @@
+namespace Vidya.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addConventionsToCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String());
+        }
+    }
+}
